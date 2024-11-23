14385363ff544b80f9948f4d4ea5fee89785033f
fatimamelo <fatimabsmelo@hotmail.com>
2024-11-22 21:52:18 -0300

    copiaaa

--- principal/urls.py
+++ principal/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import findex
+from .views import findex, fhistoria
 
 urlpatterns = [
-    path('', findex),
+    path('', findex), #chama direto pelo servidor
+    path('fhistoria/',fhistoria, name='fhistoria'),
+    path('findex/',findex, name='findex'), #cria link de url
 
 
 
--- principal/views.py
+++ principal/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def findex(request):
     return render(request, "index.html")
+
+def fhistoria(request):
+    return render(request, "historia.html")
